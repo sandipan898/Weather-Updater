caab22891ba1de2627d8efb6937ba8c57c375396
SandipanDas <50514440+sandipan898@users.noreply.github.com>
2020-04-03 04:20:40 -0700

    Update weather_updater2.py

--- weather_app2.py
+++ weather_app2.py
@@ -1,22 +1,31 @@
 import tkinter as tk
 import requests
 import json
+import textwrap as tw
 
 root = tk.Tk()
 root.title("Weather Forecast")
-root.geometry("700x300")
+root.geometry("620x250+720+70")
 root.configure(background = 'light green')
 
 # http://dataservice.accuweather.com/forecasts/v1/daily/1day/191578?apikey=CFfODWB5pWrrIs0uMBjhGeP5uunW09D9&language=en-us&details=false&metric=false
 
+"""
+# get the response by request
 response = requests.get("http://dataservice.accuweather.com/forecasts/v1/daily/1day/191578?apikey=CFfODWB5pWrrIs0uMBjhGeP5uunW09D9&language=en-us&details=false&metric=false")
 info = json.loads(response.content)
-# print(info)
-"""
+
+# used to save the information get by request
 with open("acuuweather_info_local_forecast.txt", "w") as f:
     f.write(str(info))
-    f.close()
+    f.close() 
 """
+
+# used when more requests can not be possible
+with open("acuuweather_info_local_forecast.txt", "r") as f:
+    info = f.read()
+    f.close()
+info = eval(info)
 
 info_headLine = info['Headline']
 info_DailyForecasts = info['DailyForecasts']
@@ -28,30 +37,37 @@ max_temp = temperature['Maximum']['Value']
 day_info = info_DailyForecasts[0]['Day']
 night_info = info_DailyForecasts[0]['Night']
 
-
-def rain_sts_checker():
-
+def prec_sts_checker():
     if info_DailyForecasts[0]['Day']['HasPrecipitation']:
-        return "In Day: " + info_DailyForecasts[0]['Day']['PrecipitationType']
+        return "In Day: " + info_DailyForecasts[0]['Day']['PrecipitationIntensity'] + " " + info_DailyForecasts[0]['Day']['PrecipitationType'] + "\n"
 
     if info_DailyForecasts[0]['Night']['HasPrecipitation']:
-        return "In night : " + info_DailyForecasts[0]['Night']['PrecipitationType']
+        return "In night : " + info_DailyForecasts[0]['Night']['PrecipitationIntensity'] + " " + info_DailyForecasts[0]['Night']['PrecipitationType'] + "\n"
 
+location_label1 = tk.Label(root, font = ("Helvetica", 14, "bold"), background = "light green", text = "Location : " )
+location_label2 = tk.Label(root, font = ("Helvetica", 10, "bold"), background = "light green", text = "@local")
+temp_label1 = tk.Label(root, font = ("Helvetica", 14, "bold"), background = "light green", text = "Temnpreature: ")
+temp_label2 = tk.Label(root, font = ("Helvetica", 10, "bold"), background = "light green", text = "Minimum : " + str(min_temp) + " F" + "   ||   " + "Maximum : " + str(max_temp) + " F")
+current_sts1 = tk.Label(root, font = ("Helvetica", 14, "bold"), background = "light green", text = "Today's Info: ")
+current_sts2 = tk.Label(root, font = ("Helvetica", 10, "bold"), background = "light green", text = "Day : " + day_info['IconPhrase'] + "\n\n" + "Night : " + night_info['IconPhrase'])
+prec_sts_label1 = tk.Label(root, font = ("Helvetica", 14, "bold"), background = "light green", text = "Precipittion Status : ")
+prec_sts_label2 = tk.Label(root, font = ("Helvetica", 10, "bold"), background = "light green", text = prec_sts_checker())
+sts_label1 = tk.Label(root, font = ("Helvetica", 14, "bold"), background = "light green", text = "Status :")
+sts_label2 = tk.Label(root, font = ("Helvetica", 10, "bold"), background = "light green", text = tw.fill(Text, width = 40))
+sts_type_label = tk.Label(root, font = ("Helvetica", 11, "bold"), background = "light green", text = "Type: " + category)
+# print(tw.fill(Text))
 
-try:
-    temp_label = tk.Label(root, background = "light green", font = ("Helvetica", 10, "bold"), text = "Temnpreature\n" + "Minimum : " + str(min_temp) + " F" + "   ||   " + "Maximum : " + str(max_temp) + " F")
-    temp_label.pack(pady = 10)
-
-    d_n_sts_label = tk.Label(root, background = "light green", font = ("Helvetica", 10, "bold"), text = "Day : " + day_info['IconPhrase'] + "\n\n" + "Night : " + night_info['IconPhrase'])
-    d_n_sts_label.pack(pady = 10)
-
-    rain_sts_label = tk.Label(root, background = "light green", font = ("Helvetica", 10, "bold"), text = "Rain status" + " \n" + rain_sts_checker())
-    rain_sts_label.pack()
-
-    status_label = tk.Label(root, background = "light green", font = ("Helvetica", 10, "bold"), text = "Status : " + Text + "\n\n" + "Type: " + category)
-    status_label.pack(pady = 10)
-except Exception as e:
-    error_lable = tk.Label(root, text = "Error!!!")
-    error_lable.pack()
+location_label1.grid(row = 0, column = 0, padx = 10, pady = 5)
+location_label2.grid(row = 0, column = 1, padx = 10, pady = 5)
+temp_label1.grid(row = 1, column = 0, padx = 10, pady = 0)
+temp_label2.grid(row = 1, column = 1, padx = 5, pady = (5, 0))
+current_sts1.grid(row = 2, column = 0, padx = 5, pady = 0)
+current_sts2.grid(row = 2, column = 1, padx = 5, pady = (10, 0))
+prec_sts_label1.grid(row = 3, column = 0, padx = (20, 0), pady = 0)
+prec_sts_label2.grid(row = 3, column = 1, padx = 10, pady = (10,0))
+sts_label1.grid(row = 4, column = 0, padx = 10, pady = 0)
+sts_label2.grid(row = 4, column = 1, padx = 10, pady = (10, 0))
+sts_type_label.grid(row = 5, column = 1, padx = 10, pady = (2, 0))
 
 root.mainloop()
+
